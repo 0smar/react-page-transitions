94febfa0f1fc68775e3269971157105f47bd4c55
Osmar Ramirez <osmar.roramas@gmail.com>
2019-11-23 02:06:19 -0600

    Base Project

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Title from '../components/Title'
+
+const About = () => {
+  return (
+    <div className="inner">
+      <Title lineContent='This is the' lineContent2='About page' />
+      <div>
+        <p className="info">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati nostrum veritatis id qui impedit nemo distinctio repellendus enim, nesciunt hic ratione deserunt magnam similique error ipsa, aliquam dignissimos neque. Illum commodi sunt saepe quidem laboriosam. Quod ipsum aut fugit. Possimus ipsum.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default About;
---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Title from '../components/Title'
+
+const Home = () => {
+  return (
+    <div className="inner">
+      <Title lineContent='This is the' lineContent2='Home page' />
+      <div>
+        <p className="info">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati nostrum veritatis id qui impedit nemo distinctio repellendus enim, nesciunt hic ratione deserunt magnam similique error ipsa, aliquam dignissimos neque. Illum commodi sunt saepe quidem laboriosam. Quod ipsum aut fugit. Possimus ipsum.
+        </p>
+      </div>
+    </div>
+  )
+};
+
+export default Home;
